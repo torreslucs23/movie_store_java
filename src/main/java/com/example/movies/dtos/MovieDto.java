a9becf27fbbd6afcffdba1daf0d98107bd792c2f
torreslucs23 <torreslucs23@gmail.com>
2024-01-16 14:13:02 -0300

    created crud for spring

--- src/main/java/com/example/movies/dtos/MovieDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.movies.dtos;
-
-
-import org.antlr.v4.runtime.misc.NotNull;
-
-public record MovieDto( String name,  String director,
-                              int year) {
-}
