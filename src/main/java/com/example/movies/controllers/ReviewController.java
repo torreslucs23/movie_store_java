f4342784afdc309156c8e218bb4bac33fc2e8513
torreslucs23 <torreslucs23@gmail.com>
2024-02-05 15:34:01 -0300

    hot fix endpoint review messages

--- src/main/java/com/example/movies/controllers/ReviewController.java
+++ src/main/java/com/example/movies/controllers/ReviewController.java
@@ -1,5 +1,6 @@
 package com.example.movies.controllers;
 
+import com.example.movies.dtos.ResponseDto;
 import com.example.movies.dtos.ReviewDto;
 import com.example.movies.models.Review;
 import com.example.movies.services.ReviewService;
@@ -9,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/reviews")
@@ -24,49 +28,58 @@ public class ReviewController {
 
     @PreAuthorize("hasRole('default_user')")
     @GetMapping("/{idMovie}/user/{idUser}")
-    public ResponseEntity<Review> getRatingReview(@PathVariable Long idMovie, @PathVariable Long idUser) {
+    public ResponseEntity<?> getRatingReview(@PathVariable Long idMovie, @PathVariable Long idUser) {
 
         Review review = reviewService.findReviewByIdMovieAndUser(idMovie, idUser);
 
         if (review != null) {
+            ReviewDto response = new ReviewDto(idMovie, idUser, review.getRating());
 
-            return new ResponseEntity<>(review, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            ResponseDto response = new ResponseDto("Error", "review not found");
+            return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
         }
     }
 
     @PreAuthorize("hasRole('default_user')")
     @PostMapping
-    public ResponseEntity<ReviewDto> createReview(@RequestBody ReviewDto request){
+    public ResponseEntity<?> createReview(@RequestBody ReviewDto request){
         Review createdReview = reviewService.createReview(request.movieId(), request.userId(), request.rating());
         if(createdReview != null){
             ReviewDto response = new ReviewDto(request.movieId(), request.userId(), request.rating());
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        ResponseDto response = new ResponseDto("Error", "review not found");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+
+
     @PreAuthorize("hasRole('default_user')")
     @PutMapping("/{id}")
-    public ResponseEntity<ReviewDto> updateReview(@RequestBody ReviewDto request, @PathVariable Long id){
+    public ResponseEntity<?> updateReview(@RequestBody ReviewDto request, @PathVariable Long id){
         Review review = reviewService.updateReview(request, id);
         if(review != null){
             ReviewDto response = new ReviewDto(request.movieId(), request.userId(), request.rating());
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        ResponseDto response = new ResponseDto("Error", "review not found");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
 
 
     @PreAuthorize("hasRole('default_user')")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReview(@PathVariable Long id){
+    public ResponseEntity<ResponseDto> deleteReview(@PathVariable Long id) {
         boolean deleted = reviewService.deleteReview(id);
-        return deleted ?
-                new ResponseEntity<>(HttpStatus.NO_CONTENT) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (deleted){
+            ResponseDto response = new ResponseDto("success", "Review deleted successfully");
+            return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+        }
+        else{
+            ResponseDto response = new ResponseDto("error", "Review not found");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
     }
-
-
 }
